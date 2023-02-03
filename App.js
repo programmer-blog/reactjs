e3376019b15ee2a08e024f4c8145a8e706972853
mazeqbal <programmerblog.net@gmail.com>
2023-02-03 20:18:38 +0400

    Add parcel, react, react-dom, remove jsx

--- App.js
+++ App.js
@@ -1,3 +1,24 @@
+/*** PARCEL Bundler
+ * HDR - Hot Module Replacement
+ * Fule Watcher Algorithm - C++
+ * BUNDLING
+ * MINIFY
+ * Dev and Production Build
+ * Super Fast build algorithm
+ * Image Optimization
+ * Caching While Development 
+ * Compression
+ * Compatible with older version of browsers
+ * add --https in from for nxp parcel index.html --https to run it locally as https on dev
+ * port number
+ * Consistant Hashing Algo
+ * Zero Config
+ * 
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 const heading = React.createElement("h1", { "id": "title" }, "Learning React");
 
 const heading2 = React.createElement("h2", { "id": "title" }, "Heading 2");
@@ -7,7 +28,6 @@ const container = React.createElement(
     { id: "container" },
     [heading, heading2]
 );
-console.log(heading)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(container);
